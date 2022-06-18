9583df07432f3266dc993d774d9edbd65e449b1f
isuh88 <isuh88@snu.ac.kr>
2022-06-18 23:32:51 +0900

    all games

--- participant/my_own_player.py
+++ participant/my_own_player.py
@@ -25,42 +25,56 @@ class my_own_player(part.Participant):
         # you can override this method in this sub-class
         # you can refer to an object of 'marbles', named as 'playground_marbles'
         # the return should be the number of marbles bet (> 0)!
-        my_current_marbles = playground_marbles.get_num_of_my_marbles(self)
-        return random.randint(playground_marbles.MIN_HOLDING, my_current_marbles)
+
+        return 1
 
     def declare_statement_strategy(self, playground_marbles):
         # you can override this method in this sub-class
         # you can refer to an object of 'marbles', named as 'playground_marbles'
         # the return should be True or False!
-        answer = bool(random.randint(0, 1))
+
+        if playground_marbles._marbles_in_hand % 2 == 0:
+            answer = False
+        else:
+            answer = True
         return self.set_statement(answer)
     # ================================================================================= for marble game
 
 
     # ================================================================================= for glass_stepping_stones game
+    real_answer = []    # 내 경로가 저장될 list
     def step_toward_goal_strategy(self, playground_glasses):
         # you can override this method in this sub-class
         # you can refer to an object of 'glass_stepping_stones', named as 'playground_glasses'
         # the return should be 0 or 1 (int)!
-        if self.position == 0:
-            self.temp_list = copy.deepcopy(playground_glasses._players_steps)  # 상대방것도 복사
-        length = len(self.temp_list)
-        if self.previous_player != 'None' and self.temp_list != []:
-            if self.position < length - 1:  # 카피 한 것보다 앞에 있으면
-                print(self.temp_list)
-                # print('chk1')
-                return self.temp_list[self.position]  # 내가 갔던 곳으로
-            else:
-                if self.position == length - 1:
-                    # print('chk2')
-                    if self.temp_list[self.position] == 0:
-                        return 1
-                    else:
-                        return 0
+
+        if playground_glasses._round == 1:
+            self.real_answer = []
+            first_step = random.randint(0, 1)
+            self.real_answer.append(first_step)
+            return first_step   # 첫 번째 징검다리는 random
+        else:
+            if len(self.real_answer) < len(playground_glasses._players_steps):
+                self.real_answer = copy.deepcopy(playground_glasses._players_steps)
+                # 상대가 나보다 많이 진행했을 경우 상대의 경로 복사
+            length = len(self.real_answer)
+            length_com = len(playground_glasses._players_steps)
+            if self.position < length - 1:
+                return self.real_answer[self.position]  # 상대가 떨어지기 전 단계까지는 똑같이 진행
+            elif self.position == length - 1:
+                if self.real_answer[self.position] == 0:
+                    self.real_answer[self.position] = 1
                 else:
-                    # print('chk3')
-                    return random.randint(0, 1)
-        return 'error'
+                    self.real_answer[self.position] = 0
+                return self.real_answer[self.position]  # 상대가 떨어졌던 곳에서 다른 징검다리 선택
+            elif self.position == length:
+                self.real_answer.append(random.randint(0, 1))
+                if length >= length_com:    # 내가 상대보다 멀리 진행했을 경우
+                    if self.real_answer[0] == 0:
+                        playground_glasses._players_steps[0] = 1
+                    else:
+                        playground_glasses._players_steps[0] = 0    # 상대가 참고할 내 경로의 출발지점을 바꿔버리기
+                return self.real_answer[self.position]  # 그 이후는 random
     # ================================================================================= for glass_stepping_stones game
 
 
@@ -76,24 +90,40 @@ class my_own_player(part.Participant):
         # type4 corresponds a person with strong power
         # the return should be a tuple with size of 4, and the sum of the elements should be 10
         # only for computer, it is allowed to set 12 members
-        return (0, 0, 5, 5)
+
+        return (0, 1, 0, 9)
+        # 수적 열세인 상황에서 일반적인 힘겨루기로는 승리 가능성이 낮다 전망, 넘어진 상태를 최대한 활용하여 승리 모색
+        # computer 진영을 넘어뜨려야 하기에, 상대가 높은 stamina를 return할 때 최대한 많이 끌려가야 함(heavy person 비효율적)
+        # 상대를 넘어뜨린 이후 최대한 많이 끌어와야 하므로 비슷한 몸무게 조건에서 힘이 더 쎈 사람 필요함(ordinary person 비효율적)
+        # tall person 1명 추가함으로써 전체적인 strength에 크게 영향이 없는 선에서 tugging range, maximum dragging distance 증가
 
     def act_tugging_strategy(self, playground_tug_of_war):
         # you can override this method in this sub-class
         # you can refer to an object of 'tug_of_war', named as 'playground_tug_of_war'
         # the return should be a float value in [0, 100]!
         # note that the float represents a stamina-consuming rate for tugging
-        if playground_tug_of_war.player_condition['Computer'] == False:
-            if playground_tug_of_war.player_expression[self.name] in ['best', 'well']:
-                return 15
-            else:
-                return 10
+
+        opponent_con = playground_tug_of_war.player_condition["Computer"]
+        myself_con = playground_tug_of_war.player_condition[self.name]
+        opponent_exp = playground_tug_of_war.player_expression["Computer"]
+        myself_exp = playground_tug_of_war.player_expression[self.name]
+
+        if myself_con == False:
+            return 0 # 넘어진 상태에서는 stamina 투자에 따른 net force 기댓값이 현저히 낮으므로 stamina 보전 목적
         else:
-            if playground_tug_of_war.player_expression['Computer'] in ['best', 'well']:
-                return 0
+            if opponent_con == False:
+                return 40 # 상대가 넘어졌을 때 최대한 끌어오기
             else:
-                if playground_tug_of_war.player_expression[self.name] in ['best', 'well']:
-                    return 30 + random.randint(0, 3)
+                if myself_exp == 'worst':
+                    if opponent_exp in ['best', 'well']:
+                        return 0 # stamina 회복 및 상대의 넘어짐 유도
+                    else:
+                        return 10 # 상대의 stamina가 낮을 때 반환하는 return이 적을 것으로 예상, configuration 특징 활용해 승부
+                elif myself_exp == 'bad':
+                    if opponent_exp in ['best', 'well']:
+                        return 0 # stamina 회복 및 상대의 넘어짐 유도
+                    else:
+                        return 20 # 상대의 stamina가 낮을 때 반환하는 return이 적을 것으로 예상, configuration 특징 활용해 승부
                 else:
-                    return random.randint(0, 3)
+                    return 10 # bad, worst 상태가 될 때까지 stamina를 지속적으로 소모함
     # ================================================================================= for tug_of_war game
